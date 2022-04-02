db918a7c58c72317c1e7dd2ceb8c678d4e91c27b
dsaxenaa <divyanshssaxena1@gmail.com>
2022-04-02 19:48:20 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">
+            <Link to="/" >Movies TMDB</Link>
+          </div>
+
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Watch List</Link>
+            </li>
+
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+
+            <li>
+              <Link to="/add" className="btn btn-main">
+                + Add
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
